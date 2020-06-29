28a5a9d591b8ac7ffdbbc5886dff19a41ce62f9d
x-win32api <59769004+x-win32api@users.noreply.github.com>
2020-06-29 19:39:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,12 +11,3 @@ include 'Models/ProductModel.php';
 $lastNews = News::findLastNews(3);
 # подключим шаблон
 require_once(__DIR__ . '/Views/v_index.php');
-
-/* Различные вариации
-//var_dump($Product->getAll());
-var_dump(News::findById(10));
-var_dump(News::findLastNews(3));
-
-var_dump(Product::getAll());
-var_dump(News::getAll());
-*/
