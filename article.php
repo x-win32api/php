eb1ec6084c3db1702d702ba458a66a49cf51292b
x-win32api <59769004+x-win32api@users.noreply.github.com>
2020-06-29 19:41:52 +0300

    Update article.php

--- article.php
+++ article.php
@@ -2,9 +2,7 @@
 
 use Models\News;
 
-spl_autoload_register(function ($class) {
-    require __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
-});
+require(__DIR__ . '/autoload.php');
 
 # проверим пришел ли id и загрузим новость если все ок
 $article = (isset($_GET['id'])) ? News::findById((int)$_GET['id']) : null;
