10865ba128003ed1d5f07e9210dfca19bda3aa69
x-win32api <59769004+x-win32api@users.noreply.github.com>
2020-06-30 19:52:50 +0300

    Update article.php

--- article.php
+++ article.php
@@ -5,7 +5,7 @@ use Models\News;
 require(__DIR__ . '/autoload.php');
 
 # проверим пришел ли id и загрузим новость если все ок
-$article = (isset($_GET['id'])) ? News::findById((int)$_GET['id']) : null;
+$article = (isset($_GET['id'])) ? News::findById((int)$_GET['id']) : false;
 # подключаем нужный шаблон
 if ($article) {
     require_once(__DIR__ . '/Views/v_news.php');
